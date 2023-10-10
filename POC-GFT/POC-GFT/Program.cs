059d797a1e55529833863eb357bfeaeea568ca57
mauro pena <mauropn@gmail.com>
2023-10-10 10:01:01 -0300

    removendo arquivos desnecessarios

--- POC-GFT/POC-GFT/Program.cs
+++
@@ -1,8 +0,0 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
-
-builder.Services.AddAplicationServices();
-builder.Services.AddInfrastructureServices(builder.Configuration);
-builder.Services.AddPersistenceServices(builder.Configuration);
-
-app.Run();
